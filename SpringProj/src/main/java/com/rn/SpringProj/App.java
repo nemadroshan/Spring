7dcdec78166aea5a35e09d5981cfcfc6efe13bea
Roshan Nemad <nemadroshan@gmail.com>
2020-07-24 18:41:52 +0530

    removed unused import

--- SpringProj/src/main/java/com/rn/SpringProj/App.java
+++ SpringProj/src/main/java/com/rn/SpringProj/App.java
@@ -1,10 +1,7 @@
 package com.rn.SpringProj;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
 public class App 
 {
