905904afc8b356bb64b5e99eee6ef2e8354f1ea3
Roshan Nemad <nemadroshan@gmail.com>
2020-07-24 18:58:51 +0530

    leaned beanscope singleton and prototype scope

--- SpringProj/src/main/java/com/rn/SpringProj/Alien.java
+++ SpringProj/src/main/java/com/rn/SpringProj/Alien.java
@@ -1,6 +1,34 @@
 package com.rn.SpringProj;
 
 public class Alien {
+		
+	private String name;
+	private Integer age;
+	private String address;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 	public Alien() {
 	System.out.println("Aliean 0-param costructor");
@@ -8,6 +36,11 @@ public class Alien {
 	
 	public void code() {
 		System.out.println(" I am Coading");
+	}
+
+	@Override
+	public String toString() {
+		return "Alien [address=" + address + ", age=" + age + ", name=" + name + "]";
 	}
 	
 }
--- SpringProj/src/main/java/com/rn/SpringProj/App.java
+++ SpringProj/src/main/java/com/rn/SpringProj/App.java
@@ -11,13 +11,21 @@ public class App
     	ApplicationContext ctx= null;
 
        Alien alien =null;
+       Alien alien2 =null;
 //    	factory= new XmlBeanFactory(new FileSystemResource("src/main/java/com/rn/SpringProj/applicationContext.xml"));
         	ctx = new ClassPathXmlApplicationContext("com/rn/SpringProj/applicationContext.xml");//working
         //alien = (Alien) factory.getBean("alien");
         	
-        	alien = ctx.getBean("alien",Alien.class);
+			/*
+			 * alien = ctx.getBean("alien",Alien.class);
+			 * alien2=ctx.getBean("alien",Alien.class);
+			 * 
+			 * alien.setAge(12); System.out.println(alien.getAge());
+			 * 
+			 * System.out.println(alien2.getAge());
+			     	
         	
         	alien.code();
-        
+        */    
     }
 }
