61e34c7f8cf74c4a752457e90cb2e372ba857302
Roshan Nemad <nemadroshan@gmail.com>
2020-07-25 00:34:09 +0530

    @modelAttriute at param level

---
+++ SpringMVCProj6-@ModelAttribute-ParamLevel/src/main/java/com/rn/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.rn.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.rn.model.Alien;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("home requested");
+		return "index";
+	}
+
+	@RequestMapping("/addAliean")
+	public String addAlien(@ModelAttribute("alien")Alien alien) {
+		return "result";
+	}
+
+}
