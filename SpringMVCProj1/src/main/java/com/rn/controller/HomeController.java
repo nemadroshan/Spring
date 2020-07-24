8a9d0f06cd9f851d87c3ab546e956d1099b0ac83
Roshan Nemad <nemadroshan@gmail.com>
2020-07-24 22:47:27 +0530

    learned spring mvc

---
+++ SpringMVCProj1/src/main/java/com/rn/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.rn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("home requested");
+		return "index";
+	}
+
+}
