943b0d797a9c3a2ca5fbd91849e617150279f6d8
Roshan Nemad <nemadroshan@gmail.com>
2020-07-24 20:07:49 +0530

    understand the constructor injection

---
+++ SpringProj-ConstructorInjection/src/main/java/com/rn/SpringProj/Alien.java
@@ -0,0 +1,67 @@
+package com.rn.SpringProj;
+
+public class Alien {
+
+	private String name;
+	private Integer age;
+	private String address;
+
+	private Laptop laptop;
+
+//	public Alien() {
+//		System.out.println("Aliean 0-param costructor");
+//	}
+	
+	//PARAMETRICE CONSTRUCTOR
+	public Alien(String name , Laptop laptop) {
+		this.name = name;
+		this.laptop=laptop;
+	}
+
+
+
+	public Laptop getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println("Alien.setName()");
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		System.out.println("Alien.setAge()");
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void code() {
+		System.out.println(" I am Coading");
+		laptop.compile();
+	}
+
+	@Override
+	public String toString() {
+		return "Alien [address=" + address + ", age=" + age + ", name=" + name + "]";
+	}
+
+}
---
+++ SpringProj-ConstructorInjection/src/main/java/com/rn/SpringProj/Laptop.java
@@ -0,0 +1,13 @@
+
+package com.rn.SpringProj;
+
+public class Laptop {
+	public Laptop() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Laptop.Laptop()");
+	}
+
+	public void compile() {
+		System.out.println("Code is compiled");
+	}
+}
