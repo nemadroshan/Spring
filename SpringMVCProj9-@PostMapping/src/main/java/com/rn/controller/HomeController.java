0bde94fa8933993c803021316d7f1b3c8d3e9049
Roshan Nemad <nemadroshan@gmail.com>
2020-07-25 09:44:35 +0530

    @PostMapping Example

---
+++ SpringMVCProj9-@PostMapping/src/main/java/com/rn/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.rn.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.rn.model.Alien;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("home requested");
+		return "index";
+	}
+
+	// @RequestMapping(path = "/addAliean" , method = RequestMethod.POST)
+	//@RequestMapping Support both all httpMethods
+	
+	@PostMapping("/addAliean")
+	public String addAlien(@ModelAttribute("alien") Alien alien) {
+		return "result";
+	}
+
+	@ModelAttribute
+	public void addModaldata(Model model) {
+		model.addAttribute("name", "roshan");
+	}
+
+}
