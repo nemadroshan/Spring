6d546c40e0d5a92bd0beb0e0e7a46535f5e6131b
Roshan Nemad <nemadroshan@gmail.com>
2020-07-29 23:58:03 +0530

    implemented slf4j logging in @Before Advice

--- SpringProj-AOP/src/main/java/com/rn/aspect/LoggingAspect.java
+++ SpringProj-AOP/src/main/java/com/rn/aspect/LoggingAspect.java
@@ -2,13 +2,19 @@ package com.rn.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+	
+		private static Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+	
 			@Before("execution(public * com.rn.controller.HomeController.*(..))")
 			public void log() {
-				System.out.println("LoggingAspect.log()");
+				//System.out.println("LoggingAspect.log()");
+				logger.info("inside from log method : logging aspect class");
 			}
 }
