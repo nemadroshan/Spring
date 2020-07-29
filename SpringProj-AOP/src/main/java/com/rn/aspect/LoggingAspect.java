8f346e1b9f642445efee19ee081c07f119f9718c
Roshan Nemad <nemadroshan@gmail.com>
2020-07-30 00:05:21 +0530

    implemented @After Advice from AOP

--- SpringProj-AOP/src/main/java/com/rn/aspect/LoggingAspect.java
+++ SpringProj-AOP/src/main/java/com/rn/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.rn.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,8 +14,14 @@ public class LoggingAspect {
 		private static Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 	
 			@Before("execution(public * com.rn.controller.HomeController.*(..))")
-			public void log() {
+			public void logBefore() {
 				//System.out.println("LoggingAspect.log()");
-				logger.info("inside from log method : logging aspect class");
+				logger.info("inside from logBEfore method : logging aspect class");
+			}
+			
+			@After("execution(public * com.rn.controller.HomeController.*(..))")
+			public void logAfter() {
+				//System.out.println("LoggingAspect.log()");
+				logger.info("inside from logAfter method : logging aspect class");
 			}
 }
--- SpringProj-AOP/src/main/java/com/rn/controller/HomeController.java
+++ SpringProj-AOP/src/main/java/com/rn/controller/HomeController.java
@@ -51,6 +51,7 @@ public class HomeController {
 
 		List<Alien> aliens = repo.findAll();
 		map.put("result", aliens);
+		System.out.println("**********************************************");
 		return "result";
 	}
 
